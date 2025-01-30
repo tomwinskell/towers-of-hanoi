d3e0a751d85284c95c46be71b7ac31e3705ed645
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-01-30 10:33:27 -0500

    Confirmed functionality, game works and declares winner. Abstracted more code into specific functions.

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ function Peg(numOfRings = 5) {
   });
 }
 
-function Board(numOfPegs = 3) {
-  const numOfRings = 5;
-
-  this.pegs = {
-    1: new Peg(),
+function Board() {
+  this.getNumPegs = function () {
+    let numOfPegs = parseInt(prompt(messages.start));
+    if (!numOfPegs || numOfPegs < 3 || numOfPegs > 6) {
+      numOfPegs = 3;
+    }
+    return numOfPegs;
   };
 
-  Array.from({ length: numOfPegs - 1 }).map((_, index) => {
-    this.pegs[index + 2] = new Peg(0);
-  });
+  this.build = function () {
+    this.numOfPegs = this.getNumPegs();
+    this.numOfRings = 5;
+    this.pegs = {
+      1: new Peg(this.numOfRings),
+    };
 
-  this.moveDisc = function (start, end) {
+    Array.from({ length: this.numOfPegs - 1 }).map((_, index) => {
+      this.pegs[index + 2] = new Peg(0);
+    });
+  };
+
+  this.moveDisc = function (start, end, numOfRings) {
     const startRings = this.pegs[start].rings;
     const endRings = this.pegs[end].rings;
 
@@ -24,7 +34,7 @@ function Board(numOfPegs = 3) {
     if (endRings.length === 0) {
       endRing = numOfRings;
     }
-    if (startRing < endRing) {
+    if (startRing <= endRing) {
       startRings.pop();
       endRings.push(startRing);
       this.printBoard(messages.success);
@@ -33,17 +43,36 @@ function Board(numOfPegs = 3) {
     }
   };
 
-  this.loopGame = function (pegNumsStr) {
-    while (!this.winner(numOfRings)) {
+  this.createPegStr = function (numOfPegs) {
+    return [...Array(numOfPegs)]
+      .reduce((acc, _, i) => {
+        acc.push(i + 1);
+        return acc;
+      }, [])
+      .join(' ,');
+  };
+
+  this.startGame = function () {
+    this.build();
+    this.printBoard();
+    this.loopGame(
+      this.createPegStr(this.numOfPegs),
+      this.numOfRings,
+      this.numOfPegs
+    );
+  };
+
+  this.loopGame = function (pegNumsStr, numOfRings, numOfPegs) {
+    while (!this.winner(numOfRings, numOfPegs)) {
       let start;
       let quit;
-      while (!start && !quit || start < 0 || start > numOfRings) {
+      while ((!start && !quit) || start < 0 || start > numOfRings) {
         start = prompt(`Enter ${pegNumsStr} to move ring from:
     \n(enter 'quit' to quit.)`);
         start === 'quit' ? (quit = true) : null;
       }
       let end;
-      while (!end && !quit || end < 0 || end > numOfRings) {
+      while ((!end && !quit) || end < 0 || end > numOfRings) {
         end = prompt("Move peg to:\n(enter 'quit' to quit.)");
         end === 'quit' ? (quit = true) : null;
       }
@@ -53,9 +82,8 @@ function Board(numOfPegs = 3) {
         break;
       }
 
-      this.moveDisc(start, end);
+      this.moveDisc(start, end, numOfRings);
     }
-    this.winner(numOfRings);
   };
 
   this.printBoard = function (string = 'The board is currently:') {
@@ -72,7 +100,7 @@ function Board(numOfPegs = 3) {
     console.log(toPrint);
   };
 
-  this.winner = function (numOfRings) {
+  this.winner = function (numOfRings, numOfPegs) {
     const finalPeg = this.pegs[numOfPegs].rings;
 
     if (finalPeg.length === numOfRings) {
@@ -100,23 +128,5 @@ const messages = {
       a smaller one, board is \x1b[35mstill\x1b[0m:`,
 };
 
-const startGame = function () {
-  let numOfPegs = parseInt(prompt(messages.start));
-  if (!numOfPegs || numOfPegs < 3 || numOfPegs > 6) {
-    numOfPegs = 3;
-  }
-
-  const game = new Board(numOfPegs);
-  const pegNumsStr = [...Array(numOfPegs)]
-    .reduce((acc, _, i) => {
-      acc.push(i + 1);
-      return acc;
-    }, [])
-    .join(' ,');
-
-  game.printBoard();
-  game.loopGame(pegNumsStr);
-
-};
-
-startGame();
+const game = new Board();
+game.startGame();
