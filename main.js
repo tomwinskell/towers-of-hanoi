3fb0d8cb4c6c34208a622a59e5077c15e3f46d70
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-02-07 12:58:22 -0500

    Complete refactor. Debugged.

--- main.js
+++ main.js
@@ -1,136 +1,138 @@
 const messages = {
-  prompt: `
-  Enter number of pegs to play with.\n
+  prompt: `Enter number of pegs to play with.
   Numbers between 3 and 5 are valid.\n
   Default is 3 pegs.`,
-  moveDisc: `
-  Move all the discs to a different\n
-  peg to win.\n
-  Use command moveDisc() to move rings.\n
-  Example: moveDisc(1, 3) will move the top disc\n
-  from peg 1 to peg 3.\n
-  You cannot move a larger ring on top of one\n
-  which is smaller.`,
-  noRings: `There are no rings on the peg you\n
-  selected.`,
-  won: `You won!`,
+  moveDisc: `Move all the discs to a different peg to win.\n
+Use command moveDisc() to move rings.\n
+Example: moveDisc(1, 3) will move the top disc from peg 1 to peg 3.\n
+You cannot move a larger ring on top of one which is smaller.\n`,
+  noRings: `There are no rings on the peg you selected.`,
+  won: `You won! Reseting to a new board in 5 seconds.`,
   success: `That move was successful, board is \x1b[35mnow\x1b[0m:`,
-  unsuccess: `You cannot move a larger disc on top \x1b[35mof\x1b[0m a smaller one.`,
+  nosuccess: `You cannot move a larger disc on top \x1b[35mof\x1b[0m a smaller one.`,
 };
 
-class Peg {
-  constructor(numOfRings) {
-    this.rings = [...Array(numOfRings)].map((_, index) => numOfRings - index);
-  }
-}
-
 class Board {
-  constructor(numOfRings = 5) {
+  constructor(numOfRings = 3) {
     this.numOfRings = numOfRings;
     this.numOfPegs = prompt(messages['prompt']);
-    if (!this.numOfPegsIsValid(this.numOfPegs)) {
+    if (!this.validPegInput(this.numOfPegs)) {
       this.numOfPegs = 3;
     }
     this.build();
   }
 
-  numOfPegsIsValid(numOfPegs) {
-    if (numOfPegs.length != 1) {
-      return false;
-    }
-    if (!numOfPegs > 2 && numOfPegs < 4) {
+  validPegInput = (numOfPegs) => {
+    const regex = /^[3-5]$/;
+    if (!regex.test(numOfPegs)) {
       return false;
     }
     return true;
-  }
+  };
 
-  build() {
-    this.pegs = { 1: new Peg(this.numOfRings) };
-    Array.from({ length: this.numOfPegs - 1 }).map((_, index) => {
-      this.pegs[index + 2] = new Peg(0);
+  build = () => {
+    this.pegs = {
+      1: [...Array(this.numOfRings)].map((_, index) =>
+        parseInt(this.numOfRings - index)
+      ),
+    };
+    Array.from({ length: this.numOfPegs - 1 }).forEach((_, index) => {
+      this.pegs[index + 2] = [];
     });
-    console.log(messages['moveDisc']);
-    this.printBoardToConsole();
-  }
+    this.printToConsole(messages['moveDisc']);
+  };
 
-  printBoardToConsole() {
-    let toPrint = ['The state of the board is currently:'];
+  printToConsole = (string) => {
+    console.clear();
+    const toPrint = [];
+    if (string) {
+      toPrint.push(string);
+    }
+    toPrint.push('The state of the board is currently:');
     for (const key in this.pegs) {
-      const str = this.pegs[key].rings.reduce((str, current) => {
+      const str = this.pegs[key].reduce((str, current) => {
         str += current;
         return str;
       }, '');
       toPrint.push(`--- ${str}`);
     }
-    return toPrint.join('\n');
-  }
+    console.log(toPrint.join('\n'));
+  };
 
-  moveDisc(start, end) {
-    if (this.discInputIsValid(start)) {
-      return messages['moveDisc'];
+  // returns undefined because there is no return value
+  // is there an elegant way to handle this situation?
+  moveDisc = (start, end) => {
+    if (!this.validDiscInput(start, end)) {
+      return;
     }
-    const startPeg = this.pegs[start]['rings'];
-    const endPeg = this.pegs[end]['rings'];
-    if (startPeg.length === 0) {
-      return messages['noRings'];
-    }
-
+    const startPeg = this.pegs[start];
+    const endPeg = this.pegs[end];
     const startRing = startPeg[startPeg.length - 1];
-    let endRing = endPeg[endPeg.length - 1];
-
-    if (endPeg.length === 0) {
-      endRing = this.numOfRings;
+    if (!this.validMove(startPeg, endPeg)) {
+      return;
     }
+    this.makeMove(startPeg, endPeg, startRing);
+  };
 
-    if (startRing > endRing) {
-      return messages['unsuccess'];
-    }
-    this.modifyBoard(startPeg, endPeg, startRing);
-
-    if (this.winner()) {
-      return messages['won'];
-    }
-    return messages['success'];
-  }
-
-  modifyBoard(startPeg, endPeg, startRing) {
+  makeMove = (startPeg, endPeg, startRing) => {
     startPeg.pop();
     endPeg.push(startRing);
-  }
+    if (this.winner()) {
+      this.printToConsole(messages['won']);
+      setTimeout(() => {
+        this.resetBoard();
+      }, 5000);
+    } else {
+      this.printToConsole(messages['success']);
+    }
+  };
 
-  discInputIsValid(number) {
-    if (number.length != 1) {
-      return false;
-    }
-    if (number < 1 || number > this.numOfPegs) {
-      return false;
-    }
-    if (start === end) {
+  // validates input, debugged
+  validDiscInput = (start, end) => {
+    if (
+      start < 1 ||
+      end < 1 ||
+      start > this.numOfPegs ||
+      end > this.numOfPegs ||
+      start === end
+    ) {
+      this.printToConsole(messages['moveDisc']);
       return false;
     }
     return true;
-  }
+  };
 
-  winner() {
-    Object.entries(this.pegs)
+  // validates the move, debugged
+  validMove = (startPeg, endPeg) => {
+    let endRing;
+    if (endPeg.length === 0) {
+      endRing = this.numOfRings;
+    } else {
+      endRing = endPeg[endPeg.length - 1];
+    }
+    if (startPeg.length === 0) {
+      this.printToConsole(messages['noRings']);
+      return false;
+    }
+    if (startPeg[startPeg.length - 1] > endRing) {
+      this.printToConsole(messages['nosuccess']);
+      return false;
+    }
+    return true;
+  };
+
+  // checks ALL pegs other than first for winner, debugged
+  winner = () => {
+    return Object.values(this.pegs)
       .slice(1)
-      .forEach(([key, value]) => {
-        if (value.length === this.numOfRings) {
-          return true;
-        }
-      });
-    return false;
-  }
+      .some((peg) => peg.length === this.numOfRings);
+  };
+
+  resetBoard = () => {
+    this.pegs = [];
+    this.build();
+  };
 }
 
-(() => {
-  console.log(messages['prompt']);
-})();
-
 const game = new Board();
-
-const moveDisc = (start, end) => {
-    const message = game.moveDisc(start, end);
-    const board = game.printBoardToConsole();
-};
-
+const moveDisc = game.moveDisc;
